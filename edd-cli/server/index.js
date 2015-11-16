12ed32a9a71862ca48387c4e44d2c533a89f5f1e
Benjamin Borowski <ben.borowski@typeoneerror.com>
2015-11-15 18:35:24 -0800

    Simplify "CORS" for demo

--- edd-cli/server/index.js
+++ edd-cli/server/index.js
@@ -1,12 +1,6 @@
 module.exports = function(app) {
-  var cors       = require('cors');
-
-  app.use(cors());
-
-
-  // proxy expects a stream, but express will have turned
-  // the request stream into an object because bodyParser
-  // has run. We have to convert it back to stream:
-  // https://github.com/nodejitsu/node-http-proxy/issues/180
-  app.use(require('connect-restreamer')());
+  app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  });
 };
